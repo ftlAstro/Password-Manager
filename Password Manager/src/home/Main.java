8ebd69874c35b9f2eb64c658b2d188d27a5faf0f
Fabianl7 <48733395+Fabianl7@users.noreply.github.com>
2022-09-10 21:07:45 -0400

    Add files via upload

--- Password Manager/src/home/Main.java
+++ Password Manager/src/home/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             y = event.getSceneY();
         });
         root.setOnMouseDragged(event -> {
+
             primaryStage.setX(event.getScreenX() - x);
             primaryStage.setY(event.getScreenY() - y);
 
